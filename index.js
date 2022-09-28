644021279e4de2487622ee6c873cd283ebe1fbd4
BrunaScauri <brunascauri@hotmail.com>
2022-09-28 14:10:02 -0300

    docs: add axios and cors to package.json

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
-// const axios = require("axios");
+const axios = require('axios');
+const cors = require('cors');
+
 const app = express();
 
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-	try {
-	// const response = "https://space-news.p.rapidapi.com/news/guardian"
-	// res.send(response.data)
-	res.send('hello, api!');
-	} catch(e){
-		console.error(e)
-	}
+  try {
+    // const response = "https://space-news.p.rapidapi.com/news/guardian"
+    // res.send(response.data)
+    res.send('hello, api!');
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 // axios.request(options).then(function (response) {
-// 	console.log(response.data);
+// console.log(response.data);
 // }).catch(function (error) {
-// 	console.error(error);
+// console.error(error);
 // });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
